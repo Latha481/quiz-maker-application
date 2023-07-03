5da4470017a912f60b32c7c7d2e26a253259d9c3
Latha Shankar <ls15@ford.com>
2023-07-03 07:34:20 +0530

    Path changes

--- src/app/trivia-data.service.spec.ts
+++ src/app/trivia-data.service.spec.ts
@@ -1,14 +1,13 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TriviaDataService } from './trivia-data.service';
 
 describe('TriviaDataService', () => {
   let service: TriviaDataService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TriviaDataService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [TriviaDataService]
+  }));
 
   it('should be created', () => {
     expect(service).toBeTruthy();
